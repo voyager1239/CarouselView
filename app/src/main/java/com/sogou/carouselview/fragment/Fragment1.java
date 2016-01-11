95e16e65fdfa8a7f0454e86d270b80e62d544377
lianghenghui <liangheng1239@163.com>
2016-01-11 18:22:41 +0800

    测试上下移动动画

--- app/src/main/java/com/sogou/carouselview/fragment/Fragment1.java
+++ app/src/main/java/com/sogou/carouselview/fragment/Fragment1.java
@@ -5,9 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.ValueAnimator;
 import com.sogou.carouselview.CarouselAdapter;
 import com.sogou.carouselview.CarouselViewPager;
 import com.sogou.carouselview.DotView;
@@ -23,6 +27,8 @@ public class Fragment1 extends Fragment {
     private CarouselViewPager carouselViewPager;
     private DotView dotView;
     private View rootView;
+    private Button button;
+    private int topMargin = 0;
     private final int[] images = {R.drawable.a, R.drawable.c,
             R.drawable.d, R.drawable.e};
     private ArrayList<ImageView> imageList;
@@ -49,8 +55,31 @@ public class Fragment1 extends Fragment {
     }
 
     private void initWidget() {
+        button = (Button) rootView.findViewById(R.id.bt_click);
         carouselViewPager = (CarouselViewPager) rootView.findViewById(R.id.carousel_viewpager);
         dotView = (DotView) rootView.findViewById(R.id.dot_view);
+        final LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams)button.getLayoutParams();
+
+        topMargin= lp.topMargin;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(button, "accelerate", 0.0f, 1.0f)
+                        .setDuration(500);
+                objectAnimator.start();
+                objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float val = (float) animation.getAnimatedValue();
+                        lp.setMargins(lp.leftMargin, (int) (topMargin + val * 80),
+                                lp.rightMargin, lp.bottomMargin);
+                        button.setLayoutParams(lp);
+                    }
+                });
+            }
+        });
     }
 
     private void loadCarouselView() {
